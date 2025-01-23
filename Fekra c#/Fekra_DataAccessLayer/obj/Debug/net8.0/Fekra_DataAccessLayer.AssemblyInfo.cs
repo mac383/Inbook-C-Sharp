de3070da70b631617b2fb93e50644d264b154f73
mac383 <murtadhamuhammad383@gmail.com>
2025-01-23 23:37:59 +0300

    some changes

--- Fekra c#/Fekra_DataAccessLayer/obj/Debug/net8.0/Fekra_DataAccessLayer.AssemblyInfo.cs
+++ Fekra c#/Fekra_DataAccessLayer/obj/Debug/net8.0/Fekra_DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fekra_DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2dcdcd94256da174b7efadd808a96e895960b65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f1742d1ca9e2f583596cbca7b671016acec6a07")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fekra_DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fekra_DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
