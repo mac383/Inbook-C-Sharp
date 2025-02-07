ddd6e414362d5759fba72117695ccca387b30a6d
mac383 <murtadhamuhammad383@gmail.com>
2025-02-07 23:48:16 +0300

    some changes

--- Fekra c#/Fekra_BusinessLayer/obj/Debug/net8.0/Fekra_BusinessLayer.AssemblyInfo.cs
+++ Fekra c#/Fekra_BusinessLayer/obj/Debug/net8.0/Fekra_BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fekra_BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e813236d292ea49a075261099e2a239284acaeda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27b9e6b3e167c051bb2fcf0b18144214bbcd2121")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fekra_BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fekra_BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
